fac187a52bd7a54ab3a00246e5d48a06f0d9032d
Wouter de Boer <wouter.de.boer@springest.nl>
2012-04-04 17:17:14 +0200

    Initial commit of the iDoneThis Alfred extension

---
+++ idonethis.php
@@ -0,0 +1,8 @@
+<?php
+$to      = str_replace(' ', '-', strtolower($argv[2])). '@team.idonethis.com';
+$subject = 'Re: '. $argv[2] . ' digest for ' . date("F d");
+$message = $argv[3];
+$sender = $argv[1];
+$headers = 'From: ' . $sender . "\r\n";
+
+mail($to, $subject, $message, $headers);

537f65efb2e0b605f865780b5afba3607f98607e
Rudy Jahchan <dev+rudyjahchan@carbonfive.com>
2012-04-29 09:54:31 -0700

    Use Appropriate Email Address When None Is Given

--- idonethis.php
+++ idonethis.php
@@ -1,8 +1,19 @@
 <?php
-$to      = str_replace(' ', '-', strtolower($argv[2])). '@team.idonethis.com';
-$subject = 'Re: '. $argv[2] . ' digest for ' . date("F d");
-$message = $argv[3];
+$argumentCount = count($argv);
+
 $sender = $argv[1];
 $headers = 'From: ' . $sender . "\r\n";
+
+if($argumentCount > 3) {
+  $to = str_replace(' ', '-', strtolower($argv[2])). '@team.idonethis.com';
+  $subjectIdentifier = $argv[2];
+  $message = $argv[3];
+} else {
+  $to = 'today@idonethis.com';
+  $subjectIdentifier = 'Personal';
+  $message = $argv[2];
+}
+
+$subject = 'Re: '. $subjectIdentifier . ' digest for ' . date("F d");
 
 mail($to, $subject, $message, $headers);
